b27636859b1f9ef6073e7f175474fbf21bd533c3
jAmaaya <119801608+jAmaaya@users.noreply.github.com>
2022-12-12 11:59:02 -0400

    first commit

---
+++ por.js
@@ -0,0 +1,22 @@
+let firstA = document.getElementById('a1');
+let firstB = document.getElementById('a2');
+let firstC = document.getElementById('a3');
+let firstD = document.getElementById('a4');
+
+
+
+function projectOne() {
+    return document.getElementById('js').innerHTML = "This project returns a chords progression of your choice base in the mood (happy, sad) or in your favorite gender like pop, ballads and Pop ballads! <a class='span2 invi' href='#'>Here is the link to the page !</a>";
+}
+
+function projectOneA() {
+    return document.getElementById('js').innerHTML = '';
+}
+
+
+
+firstA.onclick = projectOne;
+
+
+
+ 
